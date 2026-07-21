f87b5933264dc5cd6fb77f8c7932dc2391dc949b
yire444 <yire2005lol@gmail.com>
2026-07-21 15:49:52 -0500

    Repositorio del producto con filtros}

---
+++ src/main/java/com/peruviana/product/Repository/IProductRepository.java
@@ -0,0 +1,35 @@
+package com.peruviana.product.Repository;
+
+import com.peruviana.product.entity.ProductEntity;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface IProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    //VALIDATE DATA FOR REGISTER PRODUCT
+    boolean existsByName(String name);
+
+    //VALIDATE DATA FOR UPDATE
+    boolean existsByNameAndIdNot(String name, Long id);
+
+    //QUERIES FOR PRODUCT
+    @Query("""
+        SELECT p
+        FROM ProductEntity p
+        WHERE
+                (:name IS NULL OR p.name = :name)
+                 AND(:price IS NULL OR p.price <= :price)
+                 AND(:active IS NULL OR p.active = :active)
+        """)
+    List<ProductEntity> findByAllFilters(
+            @Param("name") String name,
+            @Param("price")BigDecimal price,
+            @Param("active") Boolean active,
+            Sort sort
+            );
+}
